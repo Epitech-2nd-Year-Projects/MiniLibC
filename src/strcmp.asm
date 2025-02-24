20b492db3c9df52533384e7aa31ff6089f9b4d40
Grégor Sternat <gregor.sternat@epitech.eu>
2025-02-24 16:58:12 +0100

    feat(strcmp): add new strcmp function

---
+++ src/strcmp.asm
@@ -0,0 +1,24 @@
+section .text
+global strcmp
+
+strcmp:
+    xor     rax, rax        ; Init rax to null to return if needed
+.loop:
+    mov     al, byte [rdi]  ; al = str1 char
+    mov     bl, byte [rsi]  ; bl = str2 char
+    cmp     al, bl          ; Compare al and bl
+    jne     .difference     ; if char al != char bl jump to difference
+    test    al, al          ; Check if end of str1 is null terminator
+    jz      .end            ; End program if end of str1 and str2
+    inc     rdi             ; rdi++ move to next char (str1)
+    inc     rsi             ; rsi++ move to next char (str2)
+    jmp     .loop           ; Repeat the loop
+
+.difference:
+    sub     al, bl          ; al - bl (calculate char diff)
+    movsx   rax, al         ; Sign extend the result
+    ret                     ; Return diff
+
+.end:
+    xor     eax, eax        ; Return 0 (bc string are equal)
+    ret
