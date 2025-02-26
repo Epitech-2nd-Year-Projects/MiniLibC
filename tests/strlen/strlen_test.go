30906ca462b637af4d63ae62700040091905fec7
gregorsternat <gregor.sternat@epitech.eu>
2025-02-26 03:44:09 +0100

    feat(strlen): adding tests for strlen

---
+++ tests/strlen/strlen_test.go
@@ -0,0 +1,21 @@
+package strlen
+
+import (
+    "testing"
+)
+
+func TestStrlen(t *testing.T) {
+    tests := []struct {
+        str string
+        expected uint64
+    }{
+        {str: "Hello", expected: 5},
+        {str: "", expected: 0},
+    }
+    for _, test := range tests {
+        result := strlen(test.str)
+        if result != test.expected {
+            t.Errorf("Output %q not equal to expected %q", result, test.expected)
+        } 
+    }
+}
