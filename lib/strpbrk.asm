1c896854e400715abeff4b41cd9c0318a3874dc9
Grégor Sternat <gregor.sternat@epitech.eu>
2025-03-01 17:23:24 +0100

    fix(strpbrk): remove infite loop

--- lib/strpbrk.asm
+++ lib/strpbrk.asm
@@ -2,33 +2,29 @@ section .text
 global strpbrk
 
 strpbrk:
-    push    r12                   ; Save r12
-    mov     r12, rsi              ; Save original accept pointer
+    xor     rax, rax             ; Init rax to null to return if needed
+    mov     r8, rsi              ; Save original accept pointer
 .first_loop:
     mov     al, byte [rdi]       ; al = curr char from s
     test    al, al               ; Check if curr char is null terminator
     jz      .not_found           ; Jump to not_found if true
-    mov     rsi, r12             ; Reset rsi to the start of accept
 
 .second_loop:
     mov     cl, byte [rsi]       ; cl = current character from accept
     test    cl, cl               ; Check if curr char is null terminator
     jz      .next_char           ; Jump to next char if end of reject
     cmp     al, cl               ; Compare s char (al) with accept char (cl)
-    je      .found               ; Jump to found if true
+    je      .found               ; If true jump to found
     inc     rsi                  ; rsi++ increment accept pointer
-    jmp     .first_loop          ; Repeat first loop
+    jmp     .second_loop          ; Repeat first loop
 
 .next_char:
     inc     rdi                  ; rdi++ (move to next char)
-    jmp     .second_loop         ; Repeat second loop
+    jmp     .first_loop         ; Repeat second loop
 
 .found:
     mov     rax, rdi             ; Set return pointer to current position in s (match found)
-    pop     r12                  ; Restore r12
     ret
 
 .not_found:
-    xor     rax, rax             ; Return null
-    pop     r12                  ; Restore r12
     ret
