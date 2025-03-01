7019d58080f274eac27748d7edd71161367f22e4
Grégor Sternat <gregor.sternat@epitech.eu>
2025-03-01 14:23:13 +0100

    feat(strcspn): adding new strcspn function

---
+++ lib/strcspn.asm
@@ -0,0 +1,28 @@
+section .text
+global strcspn
+
+strcspn:
+    xor     rax, rax        ; Init rax to null to return if needed
+
+.first_loop:
+    mov     dl, byte [rdi]  ; dl = curr char from s
+    test    dl, dl          ; Check if curr char is null terminator
+    jz      .end            ; Jump to end if true
+
+.second_loop:
+    mov     cl, byte [rsi]  ; cl = current character from reject
+    test    cl, cl          ; Check if curr char is null terminator
+    jz      .next_char      ; Jump to next char if end of reject
+    cmp     dl, cl          ; Compare s character (dl) with reject character (cl)
+    je      .end            ; Jump to end if char match
+    inc     rsi             ; rsi++ increment reject pointer
+    jmp     .second_loop    ; Repeat second loop
+
+.next_char:
+    inc     rdi             ; rdi++ (move to next char)
+    inc     rax             ; rax++ increment valid char counter
+    jmp     .first_loop     ; Repeat first loop
+
+.end:
+    ret
+
